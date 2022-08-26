bed8988eefc25aced8ed30a9a3f85d91206e11b8
Curtis Wilson <tightpin@gmail.com>
2022-08-26 16:47:21 +0000

    Added concat exercise

---
+++ concat.js
@@ -0,0 +1,15 @@
+function concat(array1, array2) {
+  let tempArray = [];
+  for (num in array1) {
+    tempArray.push(array1[num]);
+  }
+  for (num in array2) {
+    tempArray.push(array2[num]);
+  }
+  return tempArray;
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
