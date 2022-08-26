deea5afb9a806df4971a97307593dba0ee3d5835
Curtis Wilson <tightpin@gmail.com>
2022-08-26 16:42:48 +0000

    Added Last Index exercise.

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+function lastIndexOf(valueArray, value) {
+  let lastIndex = -1;
+  for (let i = 0; i < valueArray.length; i++) {
+    if (valueArray[i] == value) {
+      lastIndex = i;
+    }
+  }
+  return lastIndex;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
